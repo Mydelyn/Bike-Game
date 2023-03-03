d6f83e53d56e4d96b316b657ca6b7656907bee5c
Mydelyn Carnahan <mydelyn.carnahan@outlook.com>
2023-03-02 20:51:01 -0700

    Deleted half the code whoops

--- main.py
+++ main.py
@@ -29,49 +29,5 @@ jumping = False
 
 def draw_window(bike):  # Draws things onto the background window
     WIN.blit(FOREST, (0, 0))
-    WIN.blit(BIKE_FLAT, (bike.x, bike.y))
-    pygame.display.update()
 
-
-def bike_handle_movement(keys_pressed, bike):
-    if keys_pressed[pygame.K_a] and bike.x - VEL_X > 0:  # Left for Bike
-        bike.x -= VEL_X
-    if keys_pressed[pygame.K_d] and bike.x + VEL_X + bike.width < WIDTH:  # Right for Bike
-        bike.x += VEL_X
-
-
-def main():
-    bike = pygame.Rect(50, 400, BIKE_WIDTH, BIKE_HEIGHT)
-    clock = pygame.time.Clock()
-    run = True
-    jumping = False
-    Y_VELOCITY = JUMP_HEIGHT
-
-    while run:
-        clock.tick(FPS)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-                pygame.quit()
-                os._exit(0)
-
-        keys_pressed = pygame.key.get_pressed()
-
-        bike_handle_movement(keys_pressed, bike)
-        if keys_pressed[pygame.K_SPACE] and bike.y - VEL_Y > 0:
-            jumping = True
-        if jumping:
-            bike.y -= Y_VELOCITY
-            Y_VELOCITY -= Y_GRAVITY
-        if Y_VELOCITY < -JUMP_HEIGHT:
-            jumping = False
-            Y_VELOCITY = JUMP_HEIGHT
-
-        draw_window(bike)
-
-    main()
-
-
-if __name__ == "__main__":
-    main()
 
